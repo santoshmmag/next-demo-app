4a1feece16e0f34176643ad16a758208d22ea0ad
santosh <santosh@mmag.in>
2025-02-24 19:40:13 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,15 @@ export default function Home() {
                                     <label>Mobile Number<span>*</span></label><br />
                                     <div className="col-12">
                                         <input type="hidden" name="code" id="countryname" value="IN" className="countryname" />
-                                        <input id="phone" maxLength="10" minLength="10" name="phone" size="20" className="required number phone" type="tel" />                       
+                                        <input 
+                                          id="phone" 
+                                          maxLength="10" 
+                                          minLength="10" 
+                                          name="phone" 
+                                          size="20" 
+                                          className="required number phone" 
+                                          type="tel" 
+                                        />                       
                                     </div>
                                     <div className="clearfix"></div>                                 
                                   </div>
